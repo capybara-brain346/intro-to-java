0670932181deef632a92c416e697c9997e4f3048
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-08 12:15:18 +0530

    String operations

---
+++ StringMethods/StringOperations.java
@@ -0,0 +1,16 @@
+package StringMethods;
+
+import java.util.Locale;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        String str = "Now is the winter of our discontent";
+
+        System.out.println("String: "+str);
+        System.out.println("Length: "+str.length());
+        System.out.println("Char at 5: "+str.charAt(5));
+        System.out.println("Substring: "+str.substring(4,12));
+        System.out.println("Index of d: "+str.indexOf("d"));
+        System.out.println("Uppercase: "+str.toUpperCase());
+    }
+}
